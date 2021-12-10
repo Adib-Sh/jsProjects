da091a70122a71682a6e7d895d584f14ba9577c0
neoadib <neoadib@gmail.com>
2021-12-10 17:39:58 +0100

    initail commit, countdown timer added

---
+++ Countdown_timer/script.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
+
+const newYears = "1 jan 2022"
+
+function countdown() {
+    const newYearsDate = new Date(newYears)
+    const currentDate  = new Date()
+    
+
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / (3600 * 24));
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds%60)
+
+    daysEl.innerText = days
+    hoursEl.innerText = hours
+    minutesEl.innerText = minutes
+    secondsEl.innerText = seconds
+
+}
+
+setInterval(countdown, 1000)
+
+document.getElementById("days").innerText = countdown()
